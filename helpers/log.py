0aa62bc692f47e54343fcd520bd40e32fa5922a9
Lukasz Hoszowski <lukasz.hoszowski@gmail.com>
2022-09-02 05:20:25 +0200

    SFW_005-log_docs_and_typing

--- helpers/log.py
+++ helpers/log.py
@@ -1,9 +1,17 @@
-import time
 import logging
 import logging.handlers
+from collections.abc import Callable
+from multiprocessing import Queue
+from typing import NoReturn
 
 
-def listener_configurer():
+def listener_configurer() -> NoReturn:
+    """
+        Creates instance of logger for log listener, set the configuration for logger.
+
+            Returns:
+                None
+    """
     root = logging.getLogger()
     handler = logging.handlers.RotatingFileHandler('./logs/filewatcher_multi.log', 'a', 1_000_000, 3)
     formatter = logging.Formatter('%(asctime)-20s| %(levelname)-8s| %(processName)-12s| %(message)s',
@@ -12,7 +20,17 @@ def listener_configurer():
     root.addHandler(handler)
 
 
-def listener_process(configurer, queue):
+def listener_process(configurer: Callable, queue: Queue) -> NoReturn:
+    """
+        Intercepts logs from the queue and send them to handlers.
+
+            Parameters:
+                configurer (Logger): instance of listener logger object
+                queue (Queue): container for logs from independent processes
+
+            Returns:
+                None
+    """
     configurer()
     while True:
         try:
@@ -27,7 +45,15 @@ def listener_process(configurer, queue):
             traceback.print_exc(file=sys.stderr)
 
 
-def worker_configurer(queue):
+def worker_configurer(queue: Queue) -> NoReturn:
+    """
+        Creates instance of logger for workers, set the configuration for logger, store logs in shared queue.
+            Parameters:
+                queue (Queue): container for logs from independent processes
+
+            Returns:
+                None
+    """
     handler_q = logging.handlers.QueueHandler(queue)
     handler_s = logging.StreamHandler()
     
@@ -44,41 +70,3 @@ def worker_configurer(queue):
     root.addHandler(handler_q)
     root.addHandler(handler_s)
     root.setLevel(logging.INFO)
-
-
-def log_start(func_name, logger):
-    return logger.info(f'{func_name} started')
-
-
-def log_module_start(logger):
-    return logger.info(f'Main refresh started')
-
-
-def log_module_end(logger):
-    return logger.info(f'Main refresh finished')
-
-
-def log_source_files(func_name, file_names, df, logger):
-    
-    try:
-        shape = df.sheets
-    except Exception:
-        shape = 'sheets'
-        
-    return logger.debug(f'{func_name} sourced files: {file_names} readed to df -> {shape}')
-
-
-def log_etl(func_name, logger):
-    return logger.debug(f'{func_name} ETL process finished')
-
-
-def log_duration(t0):
-    return round(time.time() - t0, 1)
-
-
-def log_saved(func_name, df, t0, logger):
-    return logger.info(f'{func_name} -> {df.shape} finished in {log_duration(t0)} sec')
-
-
-def log_file_not_found(logger):
-    return logger.exception(f'!FATAL ERROR! file/folder not found, check path configuration')
